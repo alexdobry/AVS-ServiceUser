d3af714fb863863df10bacc2462f164865ab4263
Alexander Dobrynin <alexdobry@pip06.inf.fh-koeln.de>
2013-10-29 16:19:38 +0100

    2nd Day: Primzahl parallelisiert und threadsafety implementiert.

--- DObjectsTest/MathProtocol.h
+++ DObjectsTest/MathProtocol.h
@@ -1,13 +1,15 @@
 //
-//  Server.h
+//  MathProtocol.h
 //  DObjectsTest
 //
-//  Created by Patrick Englert on 22.10.13.
+//  Created by Alexander Dobrynin on 29.10.13.
 //  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
-@interface Server : NSObject
+@protocol MathProtocol <NSObject>
+
+-(int) isPrime:(int)number;
 
 @end
--- DObjectsTest/xyzAppDelegate.m
+++ DObjectsTest/xyzAppDelegate.m
@@ -7,39 +7,70 @@
 //
 
 #import "xyzAppDelegate.h"
+#import "MathProtocol.h"
 
 @implementation xyzAppDelegate
 
 @synthesize window = _window;
 
-@protocol BackupServerProtocol
-- (int)backup: (int) zahl;
-@end;
-
 - (void)dealloc
 {
     [super dealloc];
 }
+
+NSLock* myLock;
+static unsigned int counter = 2;
+
+- (int) incrementValue
+{
+    int newNumber;
+    [myLock lock];
+    newNumber = counter++;
+    [myLock unlock];
+    return newNumber;
+}
 	
+- (void)spawnThread:(NSString*)portName
+{
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    
+    id mathProtocol;
+    NSSocketPort *port = (NSSocketPort *) [[NSSocketPortNameServer sharedInstance] portForName:portName host:@"*"];
+    NSConnection *theConnection = [NSConnection connectionWithReceivePort:nil sendPort:port];
+    mathProtocol = [[theConnection rootProxy] retain];
+    [mathProtocol setProtocolForProxy:@protocol(MathProtocol)];
+    
+    int currentNumber = [self incrementValue];
+    
+    while ( currentNumber < 2100000000 ){
+        if ([mathProtocol isPrime:currentNumber]) {
+            NSLog(@"%d ist eine Primzahl", currentNumber);
+        }
+        currentNumber = [self incrementValue];
+    }
+
+    
+    [pool release];
+}
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-    //NSConnection *theConnection;
-    id backupServer;
-    
-    NSSocketPort *port = (NSSocketPort *) [[NSSocketPortNameServer sharedInstance] portForName:@"doug" host:@"*"];
-    NSConnection *theConnection = [NSConnection connectionWithReceivePort:nil sendPort:port];
-    backupServer = [[theConnection rootProxy] retain];
-    [backupServer setProtocolForProxy:@protocol(BackupServerProtocol)];
-    
-    int i = 2;
-    
-    while (true) {
         
-        NSLog(@"PI ist: %d", [backupServer backup:i]);
-        i++;
-    }
+    /*
+    for (int i = 2000000000; i < 2100000000; i++) {
+        if ([mathProtocol isPrime:i]) {
+            NSLog(@"%d ist eine Primzahl", i);
+        }
+    } */
     
+    for (int i = 1; i <= 3; i++) {
+        NSThread* myThread = [[NSThread alloc] initWithTarget:self
+                                                     selector:@selector(spawnThread:)
+                                                       object:[@"worker" stringByAppendingFormat:@"%d", i]];
+        [myThread start]; 
+    }
+   
     [pool drain];
 }
 
