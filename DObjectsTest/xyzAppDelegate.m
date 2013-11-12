f91571245c94307111422b1003e027be502f1b9c
Patrick Englert <patrickenglert@pip08.inf.fh-koeln.de>
2013-11-12 13:42:56 +0100

    Automatisches Verbinden der ServiceProvicer mit dem -User. Inklusive Ausfall eines Providers.

--- DObjectsTest/xyzAppDelegate.m
+++ DObjectsTest/xyzAppDelegate.m
@@ -9,6 +9,9 @@
 #import "xyzAppDelegate.h"
 #import "MathProtocol.h"
 
+
+
+
 @implementation xyzAppDelegate
 
 @synthesize window = _window;
@@ -29,24 +32,72 @@ static unsigned int counter = 2;
     [myLock unlock];
     return newNumber;
 }
+
 	
 - (void)spawnThread:(NSString*)portName
 {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
     
     id mathProtocol;
-    NSSocketPort *port = (NSSocketPort *) [[NSSocketPortNameServer sharedInstance] portForName:portName host:@"*"];
-    NSConnection *theConnection = [NSConnection connectionWithReceivePort:nil sendPort:port];
-    mathProtocol = [[theConnection rootProxy] retain];
-    [mathProtocol setProtocolForProxy:@protocol(MathProtocol)];
+
+    
+    NSConnection *theConnection = nil;
+    NSSocketPort *port = nil;
+   
+    
+    
+
     
     int currentNumber = [self incrementValue];
-    
-    while ( currentNumber < 2100000000 ){
-        if ([mathProtocol isPrime:currentNumber]) {
-            NSLog(@"%d ist eine Primzahl", currentNumber);
-        }
-        currentNumber = [self incrementValue];
+        while ( currentNumber < 2100000000 ){
+
+            if (theConnection == nil) {
+                
+                port = (NSSocketPort *) [[NSSocketPortNameServer sharedInstance] portForName:portName host:@"*"];
+               
+                if (port == nil) {
+                    NSLog(@"keine verbindung zu %@", portName);
+                    sleep(5);
+                    continue;
+                } 
+                theConnection = [NSConnection connectionWithReceivePort:nil sendPort:port];
+                
+
+                [theConnection setRequestTimeout:10];
+                [theConnection setReplyTimeout:5];
+                
+                mathProtocol = [[theConnection rootProxy] retain];                
+                [mathProtocol setProtocolForProxy:@protocol(MathProtocol)];
+                
+            }
+            
+            
+            //  Der try Block ist für den Ausfall eines Service Providers nötig. Der aufruf des Distributed Objects wirft eine Exception nach Timeout.
+            @try {
+                
+                if ([mathProtocol isPrime:currentNumber]) {
+                    NSLog(@"%@: %d ist eine Primzahl", portName, currentNumber);
+                    
+                }
+                currentNumber = [self incrementValue];            }
+            @catch (NSException *exception) {
+                if ([[exception name] isEqualToString:NSPortTimeoutException])
+                {
+                    NSLog(@"Verbindung verloren: %@ %d", portName, currentNumber);
+                    [mathProtocol release];
+                    [[NSSocketPortNameServer sharedInstance] removePortForName:portName];
+                    [port release];
+                    [theConnection release];
+                    mathProtocol = nil;
+                    theConnection = nil;
+                    port = nil;
+                    sleep(120); //Muss hoch sein weil das Betriebssystem den alten Port ziemlich lange offen hält
+                }
+                
+            }
+            @finally {
+                
+            }
     }
 
     
@@ -57,6 +108,10 @@ static unsigned int counter = 2;
 {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
         
+    NSArray * providerNames;
+    
+    providerNames = [NSArray arrayWithObjects:@"pip01",@"pip02",@"pip03",@"pip04",@"pip05",@"pip06",@"pip07",@"pip08",@"pip09",@"pip10", nil];
+    
     /*
     for (int i = 2000000000; i < 2100000000; i++) {
         if ([mathProtocol isPrime:i]) {
@@ -64,10 +119,12 @@ static unsigned int counter = 2;
         }
     } */
     
-    for (int i = 1; i <= 3; i++) {
+    for (id portName in providerNames) {
+    
+        
         NSThread* myThread = [[NSThread alloc] initWithTarget:self
                                                      selector:@selector(spawnThread:)
-                                                       object:[@"worker" stringByAppendingFormat:@"%d", i]];
+                                                       object:portName];
         [myThread start]; 
     }
    
